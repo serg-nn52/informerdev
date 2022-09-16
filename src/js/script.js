6635fa3efab930b38538e25f14a03c672898b2c5
Sergey Lushkin <s.lushkin@websecret.by>
2022-09-16 23:42:42 +0300

    add popup, logic, scripts, responsive layout

--- src/js/script.js
+++ src/js/script.js
@@ -1,4 +1,25 @@
 const controls = document.querySelectorAll(".controls");
+const accept = document.querySelector("#accept");
+const ok = document.querySelector("#ok");
+const okWrapper = document.querySelector(".ok");
+const send = document.querySelector("#send");
+const form = document.querySelector(".form");
+const popupTitle = document.querySelector(".popup__title");
+const popup = document.querySelector(".popup");
+const contactUs = document.querySelector(".footer__contact");
+const closePopup = document.querySelector(".popup__close");
+
+const removeValues = () => {
+  form.name.value = form.email.value = form.message.value = null;
+  ok.style.display = "none";
+  accept.checked = false;
+};
+
+const removeErrors = () => {
+  const errors = [...document.querySelectorAll(".error")];
+  console.log(errors);
+  errors.forEach((el) => el.classList.remove("error"));
+};
 
 for (let i = 0; i < controls.length; i++) {
   const slider = tns({
@@ -23,3 +44,54 @@ for (let i = 0; i < controls.length; i++) {
     },
   });
 }
+
+accept.addEventListener("input", () => {
+  accept.checked ? (ok.style.display = "block") : (ok.style.display = "none");
+});
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  const data = new FormData(form);
+  const sendData = {
+    name: data.get("name"),
+    email: data.get("email"),
+    message: data.get("message"),
+    accept: data.get("accept"),
+  };
+
+  sendData.name.trim().length < 2
+    ? form.name.classList.add("error")
+    : form.name.classList.remove("error");
+  sendData.message.trim().length < 10
+    ? form.message.classList.add("error")
+    : form.message.classList.remove("error");
+
+  !/^[A-Z0-9._%+-]+@[A-Z0-9-]+\.[A-Z]{2,4}$/i.test(sendData.email)
+    ? form.email.classList.add("error")
+    : form.email.classList.remove("error");
+
+  !sendData.accept
+    ? okWrapper.classList.add("error")
+    : okWrapper.classList.remove("error");
+
+  if (!document.querySelectorAll(".error").length) {
+    removeValues();
+    popupTitle.innerText = "Sending!";
+    popupTitle.style.color = "#b71c1c";
+    popupTitle.style.textAlign = "center";
+  }
+});
+
+contactUs.addEventListener("click", () => {
+  popup.classList.add("popup_enable");
+  document.body.classList.add("freeze");
+});
+
+popup.addEventListener("click", (e) => {
+  if (e.target === e.currentTarget || e.target === closePopup) {
+    popup.classList.remove("popup_enable");
+    document.body.classList.remove("freeze");
+    removeValues();
+    removeErrors();
+  }
+});
